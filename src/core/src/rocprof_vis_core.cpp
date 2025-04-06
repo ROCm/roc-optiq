b904e809e2d7f81bf9058ddf14b193b2fa5188ad
msattert <Mark.Satterthwaite@amd.com>
2025-04-06 17:26:29 -0400

    Setup the Windows specific log sinks.

--- src/core/src/rocprof_vis_core.cpp
+++ src/core/src/rocprof_vis_core.cpp
@@ -7,6 +7,10 @@
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/ringbuffer_sink.h"
+#if defined(_WIN32)
+#include "spdlog/sinks/msvc_sink.h"
+#include "spdlog/sinks/wincolor_sink.h"
+#endif
 
 static std::weak_ptr<spdlog::sinks::ringbuffer_sink_mt> g_rpv_log_ringbuffer;
 
@@ -19,21 +23,32 @@ extern "C"
         {
             is_inited = true;
 
-            auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-            console_sink->set_level(spdlog::level::debug);
+            std::vector<std::shared_ptr<spdlog::sinks::sink>> sinks;
 
             auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt", true);
             file_sink->set_level(spdlog::level::debug);
+            sinks.push_back(file_sink);
 
             auto buffer_sink = std::make_shared<spdlog::sinks::ringbuffer_sink_mt>(1000);
             buffer_sink->set_level(spdlog::level::debug);
+            sinks.push_back(buffer_sink);
+
+#if defined(_WIN32)
+            auto msvc_sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+            msvc_sink->set_level(spdlog::level::debug);
+            sinks.push_back(msvc_sink);
+
+            auto console_sink = std::make_shared<spdlog::sinks::wincolor_stderr_sink_mt>(
+                spdlog::color_mode::automatic);
+            console_sink->set_level(spdlog::level::debug);
+            sinks.push_back(console_sink);
+#else
+            auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+            console_sink->set_level(spdlog::level::debug);
+            sinks.push_back(console_sink);
+#endif
 
             g_rpv_log_ringbuffer = buffer_sink;
-
-            std::vector<std::shared_ptr<spdlog::sinks::sink>> sinks;
-            sinks.push_back(console_sink);
-            sinks.push_back(file_sink);
-            sinks.push_back(buffer_sink);
 
             auto new_logger = std::make_shared<spdlog::logger>(std::move(std::string("rocprofvis-log")), sinks.begin(), sinks.end());
             new_logger->set_level(spdlog::level::debug);

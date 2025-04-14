faeaa9e0c1b324e844d5f396de2dda5e1521f3c8
Tom <tkarczew@amd.com>
2025-04-14 16:42:14 -0400

    clean up

--- src/app/inc/rocprofvis_structs.h
+++ src/app/inc/rocprofvis_structs.h
@@ -15,36 +15,6 @@ typedef struct rocprofvis_trace_event_t
     double      m_duration;
 } rocprofvis_trace_event_t;
 
-// typedef struct rocprofvis_color_by_value_t
-// {
-//     float interest_1_max;
-//     float interest_1_min;
-
-// } rocprofvis_color_by_value_t;
-
-// typedef struct rocprofvis_data_point_t
-// {
-//     float xValue;
-//     float yValue;
-// } rocprofvis_data_point_t;
-
-// typedef struct rocprofvis_graph_map_t
-// {
-//     enum
-//     {
-//         TYPE_LINECHART,
-//         TYPE_FLAMECHART
-//     } graph_type;
-//     bool    display;
-//     Charts* chart;
-//     ImVec4  selected;
-
-//     bool                        color_by_value;
-//     bool                        make_boxplot;
-//     rocprofvis_color_by_value_t color_by_value_digits;
-
-// } rocprofvis_graph_map_t;
-
 typedef struct rocprofvis_trace_counter_t
 {
     double m_start_ts;

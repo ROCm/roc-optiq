49db5292eae67bc2629f3ced3eab205a5221be6f
msattert <Mark.Satterthwaite@amd.com>
2025-04-18 16:00:43 -0400

    Validate a lot more properties when loading all the entries within a track - already found a couple of bugs.

--- src/controller/tests/basic_tests.cpp
+++ src/controller/tests/basic_tests.cpp
@@ -125,6 +125,19 @@ TEST_CASE_PERSISTENT_FIXTURE(RocProfVisControllerFixture, "Tests for the Control
         spdlog::info("Get track max time: {0}", max_time);
         REQUIRE(result == kRocProfVisResultSuccess);
 
+        uint64_t track_num_entries = 0;
+        result = rocprofvis_controller_get_uint64(track_handle, kRPVControllerTrackNumberOfEntries, 0, &track_num_entries);
+        spdlog::info("Get track num entries: {0}", track_num_entries);
+        REQUIRE(result == kRocProfVisResultSuccess);
+        REQUIRE(track_num_entries > 0);
+
+        uint64_t track_type = 0;
+        result = rocprofvis_controller_get_uint64(track_handle, kRPVControllerTrackType, 0, &track_type);
+        spdlog::info("Get track type: {0} {1}", track_type, track_type == kRPVControllerTrackTypeEvents ? "Events" : "Samples");
+        REQUIRE(result == kRocProfVisResultSuccess);
+        REQUIRE((track_type == kRPVControllerTrackTypeEvents ||
+                track_type == kRPVControllerTrackTypeSamples));
+
         spdlog::info("Allocating Array");
         rocprofvis_controller_array_t* array = rocprofvis_controller_array_alloc(100);
         REQUIRE(array != nullptr);
@@ -151,7 +164,7 @@ TEST_CASE_PERSISTENT_FIXTURE(RocProfVisControllerFixture, "Tests for the Control
         result = rocprofvis_controller_get_uint64(array, kRPVControllerArrayNumEntries, 0, &num_results);
         spdlog::info("Get num elements loaded from track: {0}", num_results);
         REQUIRE(result == kRocProfVisResultSuccess);
-        REQUIRE(num_results > 0);
+        REQUIRE(num_results == track_num_entries);
 
         for (uint64_t i = 0; i < num_results; i++)
         {
@@ -159,6 +172,121 @@ TEST_CASE_PERSISTENT_FIXTURE(RocProfVisControllerFixture, "Tests for the Control
             rocprofvis_handle_t* entry = nullptr;
             result = rocprofvis_controller_get_object(array, kRPVControllerArrayEntryIndexed, i, &entry);
             REQUIRE(result == kRocProfVisResultSuccess);
+            REQUIRE(entry != nullptr);
+
+            switch (track_type)
+            {
+                case kRPVControllerTrackTypeEvents:
+                {
+                    uint64_t id = 0;
+                    result = rocprofvis_controller_get_uint64(entry, kRPVControllerEventId, 0, &id);
+                    spdlog::info("Element {0} event id: {1}", i, id);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+
+                    double start_ts = 0;
+                    result = rocprofvis_controller_get_double(entry, kRPVControllerEventStartTimestamp, 0, &start_ts);
+                    spdlog::info("Element {0} event start time: {1}", i, start_ts);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(start_ts >= min_time);
+                    REQUIRE(start_ts <= max_time);
+
+                    double end_ts = 0;
+                    result = rocprofvis_controller_get_double(entry, kRPVControllerEventEndTimestamp, 0, &end_ts);
+                    spdlog::info("Element {0} event end time: {1}", i, end_ts);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(end_ts >= min_time);
+                    REQUIRE(end_ts <= max_time);
+
+                    uint32_t name_length = 0;
+                    result = rocprofvis_controller_get_string(entry, kRPVControllerEventName, 0, nullptr, &name_length);
+                    spdlog::info("Element {0} event name length: {1}", i, name_length);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(name_length > 0);
+
+                    std::string name;
+                    name.resize(name_length);
+                    result = rocprofvis_controller_get_string(entry, kRPVControllerEventName, 0, const_cast<char*>(name.c_str()), &name_length);
+                    spdlog::info("Element {0} event name: {1}", i, name);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(name_length == name.size());
+
+                    uint32_t cat_length = 0;
+                    result = rocprofvis_controller_get_string(entry, kRPVControllerEventName, 0, nullptr, &cat_length);
+                    spdlog::info("Element {0} event category length: {1}", i, cat_length);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(cat_length > 0);
+
+                    std::string cat;
+                    cat.resize(cat_length);
+                    result = rocprofvis_controller_get_string(entry, kRPVControllerEventName, 0, const_cast<char*>(cat.c_str()), &cat_length);
+                    spdlog::info("Element {0} event category: {1}", i, cat);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(cat_length == cat.size());
+
+                    uint64_t num_child = 0;
+                    result = rocprofvis_controller_get_uint64(entry, kRPVControllerEventNumChildren, 0, &num_child);
+                    spdlog::info("Element {0} event num children: {1}", i, num_child);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(num_child == 0);
+                    break;
+                }
+                case kRPVControllerTrackTypeSamples:
+                {
+                    uint64_t id = 0;
+                    result = rocprofvis_controller_get_uint64(entry, kRPVControllerSampleId, 0, &id);
+                    spdlog::info("Element {0} sample id: {1}", i, id);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+
+                    uint64_t sample_type = 0;
+                    result = rocprofvis_controller_get_uint64(entry, kRPVControllerSampleType, 0, &sample_type);
+                    spdlog::info("Element {0} sample type: {1}", i, sample_type);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(sample_type >= kRPVControllerPrimitiveTypeUInt64);
+                    REQUIRE(sample_type <= kRPVControllerPrimitiveTypeDouble);
+
+                    double timestamp = 0;
+                    result = rocprofvis_controller_get_double(entry, kRPVControllerSampleTimestamp, 0, &timestamp);
+                    spdlog::info("Element {0} sample time: {1}", i, timestamp);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(timestamp >= min_time);
+                    REQUIRE(timestamp <= max_time);
+
+                    switch (sample_type)
+                    {
+                        case kRPVControllerPrimitiveTypeUInt64:
+                        {
+                            uint64_t value = 0;
+                            result = rocprofvis_controller_get_uint64(entry, kRPVControllerSampleValue, 0, &value);
+                            spdlog::info("Element {0} sample value: {1}", i, value);
+                            REQUIRE(result == kRocProfVisResultSuccess);
+                            break;
+                        }
+                        case kRPVControllerPrimitiveTypeDouble:
+                        {
+                            double value = 0;
+                            result = rocprofvis_controller_get_double(entry, kRPVControllerSampleValue, 0, &value);
+                            spdlog::info("Element {0} sample value: {1}", i, value);
+                            REQUIRE(result == kRocProfVisResultSuccess);
+                            break;
+                        }
+                        default:
+                        {
+                            break;
+                        }
+                    }
+
+                    uint64_t num_child = 0;
+                    result = rocprofvis_controller_get_uint64(entry, kRPVControllerSampleNumChildren, 0, &num_child);
+                    spdlog::info("Element {0} sample num children: {1}", i, num_child);
+                    REQUIRE(result == kRocProfVisResultSuccess);
+                    REQUIRE(num_child == 0);
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
         }
 
         spdlog::info("Free Future");

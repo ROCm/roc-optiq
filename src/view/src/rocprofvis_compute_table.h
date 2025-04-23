dc9032372509ef8a3b8376f218fd5d5ec23b1d17
Chen, Derek <Derek.Chen@amd.com>
2025-04-23 10:56:10 -0400

    compute-proto
    -Add metrics in Table tab.

--- src/view/src/rocprofvis_compute_table.h
+++ src/view/src/rocprofvis_compute_table.h
@@ -11,15 +11,37 @@ namespace RocProfVis
 namespace View
 {
 
+typedef enum table_view_category_t
+{
+    kTableCategorySystemSOL = 0,
+    kTableCategoryCP = 1,
+    kTableCategorySPI = 2,
+    kTableCategoryWavefrontStats = 3,
+    kTableCategoryInstructionMix = 4,
+    kTableCategoryCU = 5,
+    kTableCategoryLDS = 6,
+    kTableCategoryL1I = 7,
+    kTableCategorySL1D = 8,
+    kTableCategoryAddressProcessingUnit = 9,
+    kTableCategoryVL1D = 10,
+    kTableCategoryL2 = 11,
+    kTableCategoryL2PerChannel = 12,
+    kTableCategoryCount
+} table_view_category_t;
+
 class ComputeTableView : public RocWidget
 {
 public:
     void Render();
+    void Update();
     ComputeTableView(std::shared_ptr<ComputeDataProvider> data_provider);
     ~ComputeTableView();
 
 private:
     void RenderMenuBar();
+    void RenderMetricsTab(table_view_category_t category);
+
+    std::unordered_map<table_view_category_t, std::vector<std::unique_ptr<ComputeMetric>>> m_metrics_map;
 };
 
 }  // namespace View

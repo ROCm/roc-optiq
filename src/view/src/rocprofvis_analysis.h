95e028e32147e0dd8fed2a5c7d75bdc79cbda119
Wimalasuriyan, Sajeeth <Sajeeth.Wimalasuriyan@amd.com>
2025-04-27 16:20:33 -0400

    Push before adding analysis view

---
+++ src/view/src/rocprofvis_analysis.h
@@ -0,0 +1,33 @@
+// Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+#pragma once
+
+#include "imgui.h"
+#include <utility>
+
+namespace RocProfVis
+{
+namespace View
+{
+
+class Grid
+{
+public:
+    Grid();
+    ~Grid();
+
+    void   RenderGrid(double min_x, double max_x, double movement, float zoom,
+                      ImDrawList* draw_list, float scale_x, float v_max_x, float v_min_x,
+                      int grid_size, int sidebar_size);
+    float  GetCursorPosition();
+    double GetViewportStartPosition();
+    void   SetHighlightedRegion(std::pair<float, float> region);
+
+private:
+    float                   m_cursor_position;
+    double                  m_viewport_start_position;
+    std::pair<float, float> m_highlighted_region;
+};
+
+}  // namespace View
+}  // namespace RocProfVis

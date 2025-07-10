65b755bbdfac9bf4a25c937133323ad7411ecfc0
Tom <Tom.Karczewski@amd.com>
2025-07-10 14:45:46 -0400

    Refactored event view to use horizontal splitter widget.
    Added callstack rendering.

--- src/view/src/rocprofvis_events_view.cpp
+++ src/view/src/rocprofvis_events_view.cpp
@@ -16,75 +16,104 @@ namespace View
 {
 
 EventsView::EventsView(DataProvider& dp)
-: RocCustomWidget([] {})
-, m_data_provider(dp)
+: m_data_provider(dp)
 , m_last_selected_event(std::numeric_limits<uint64_t>::max())
-{}
+{
+    std::shared_ptr<RocWidget> left_panel =
+        std::make_shared<RocCustomWidget>([this]() { this->RenderLeftPanel(); });
+    std::shared_ptr<RocWidget> right_panel =
+        std::make_shared<RocCustomWidget>([this]() { this->RenderRightPanel(); });
+
+    LayoutItem left;
+    left.m_item           = left_panel;
+    left.m_window_padding = ImVec2(4, 4);
+    LayoutItem right;
+    right.m_item           = right_panel;
+    right.m_window_padding = ImVec2(4, 4);
+
+    m_h_spilt_container = std::make_shared<HSplitContainer>(left, right);
+    m_h_spilt_container->SetMinLeftWidth(10.0f);
+    m_h_spilt_container->SetMinRightWidth(10.0f);
+    m_h_spilt_container->SetSplit(0.6f);
+}
 
 EventsView::~EventsView() {}
 
 void
 EventsView::Render()
 {
-    ImVec2 avail      = ImGui::GetContentRegionAvail();
-    float  left_width = avail.x * 0.4f;
+    m_h_spilt_container->Render();
+}
 
-    ImGui::BeginChild("EventsViewParent", avail, true);
-
-    ImGui::BeginChild("LeftPanel", ImVec2(left_width, 0), true,
-                      ImGuiWindowFlags_NoMove );
-
-    uint64_t selected_event = m_data_provider.GetSelectedEventId();
-    if(selected_event == std::numeric_limits<uint64_t>::max())
+void
+EventsView::RenderLeftPanel()
+{
+    if(ImGui::BeginChild("LeftPanel", ImVec2(0, 0), true, ImGuiWindowFlags_NoMove))
     {
-        ImGui::Text("No event selected.");
-    }
-    else
-    {
-        ImGui::Text("Event ID: %llu", m_data_provider.GetSelectedEventId());
-        if(selected_event != m_last_selected_event)
+        uint64_t selected_event = m_data_provider.GetSelectedEventId();
+        if(selected_event == std::numeric_limits<uint64_t>::max())
         {
-            if(selected_event != std::numeric_limits<uint64_t>::max())
+            ImGui::Text("No event selected.");
+        }
+        else
+        {
+            ImGui::Text("Event ID: %llu", m_data_provider.GetSelectedEventId());
+            if(selected_event != m_last_selected_event)
             {
-                m_data_provider.FetchEventExtData(m_data_provider.GetSelectedEventId());
-                m_data_provider.FetchEventFlowDetails(m_data_provider.GetSelectedEventId());
-                m_data_provider.FetchEventCallStackData(m_data_provider.GetSelectedEventId());
+                if(selected_event != std::numeric_limits<uint64_t>::max())
+                {
+                    m_data_provider.FetchEventExtData(
+                        m_data_provider.GetSelectedEventId());
+                    m_data_provider.FetchEventFlowDetails(
+                        m_data_provider.GetSelectedEventId());
+                    m_data_provider.FetchEventCallStackData(
+                        m_data_provider.GetSelectedEventId());
+                }
+                m_last_selected_event = selected_event;
             }
-            m_last_selected_event = selected_event;
-        }
 
-        const event_info_t& eventInfo = m_data_provider.GetEventInfoStruct();
-        if(!eventInfo.ext_data.empty())
-        {
-            ImGui::Separator();
-            ImGui::NewLine();
+            const event_info_t& eventInfo = m_data_provider.GetEventInfoStruct();
+            if(!eventInfo.ext_data.empty())
+            {
+                ImGui::Separator();
+                ImGui::NewLine();
 
-            ShowEventExtDataPanel(eventInfo.ext_data);
-        }
+                RenderEventExtData(eventInfo.ext_data);
+            }
 
-        const flow_info_t& flowInfo = m_data_provider.GetFlowInfo();
-        if(!flowInfo.flow_data.empty())
-        {
-            ImGui::NewLine();
-            ImGui::Separator();
-            ShowEventFlowInfoPanel(flowInfo.flow_data);
+            const flow_info_t& flowInfo = m_data_provider.GetFlowInfo();
+            if(!flowInfo.flow_data.empty())
+            {
+                ImGui::NewLine();
+                ImGui::Separator();
+                RenderEventFlowInfo(flowInfo.flow_data);
+            }
         }
     }
-
-    ImGui::EndChild();
-
-    ImGui::SameLine();
-
-    ImGui::BeginChild("RightPanel", ImVec2(0, 0), true,
-                      ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar);
-    ImGui::Text("Event Details / Timeline / Other Content Here");
-    ImGui::EndChild();
-
     ImGui::EndChild();
 }
 
 void
-EventsView::ShowEventExtDataPanel(const std::vector<event_ext_data_t>& ext_data)
+EventsView::RenderRightPanel()
+{
+    if(ImGui::BeginChild("RightPanel", ImVec2(0, 0), true,
+                         ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar))
+    {
+        const call_stack_info_t& call_stack = m_data_provider.GetCallStackInfo();
+        if(!call_stack.call_stack_data.empty())
+        {
+            RenderCallStackData(call_stack.call_stack_data);
+        }
+        else
+        {
+            ImGui::Text("Call stack data not available");
+        }
+    }
+    ImGui::EndChild();
+}
+
+void
+EventsView::RenderEventExtData(const std::vector<event_ext_data_t>& ext_data)
 {
     ImGui::TextUnformatted("Event Extended Data");
     ImGui::Separator();
@@ -110,11 +139,10 @@ EventsView::ShowEventExtDataPanel(const std::vector<event_ext_data_t>& ext_data)
         }
         ImGui::EndTable();
     }
-
 }
 
 void
-EventsView::ShowEventFlowInfoPanel(const std::vector<event_flow_data_t>& flow_data)
+EventsView::RenderEventFlowInfo(const std::vector<event_flow_data_t>& flow_data)
 {
     ImGui::TextUnformatted("Event Flow Info");
     ImGui::Separator();
@@ -140,6 +168,35 @@ EventsView::ShowEventFlowInfoPanel(const std::vector<event_flow_data_t>& flow_da
             ImGui::TextUnformatted(std::to_string(item.track_id).c_str());
             ImGui::TableSetColumnIndex(3);
             ImGui::TextUnformatted(std::to_string(item.direction).c_str());
+        }
+        ImGui::EndTable();
+    }
+}
+
+void
+EventsView::RenderCallStackData(const std::vector<call_stack_data_t>& call_stack_data)
+{
+    ImGui::TextUnformatted("Call Stack Data");
+    ImGui::Separator();
+
+    if(ImGui::BeginTable("CallStackTable", 5,
+                         ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg |
+                             ImGuiTableFlags_Resizable))
+    {
+        ImGui::TableSetupColumn("Line");
+        ImGui::TableSetupColumn("Function");
+        ImGui::TableSetupColumn("Arguments");
+        ImGui::TableHeadersRow();
+
+        for(const auto& item : call_stack_data)
+        {
+            ImGui::TableNextRow();
+            ImGui::TableSetColumnIndex(0);
+            ImGui::TextUnformatted(item.line.c_str());
+            ImGui::TableSetColumnIndex(1);
+            ImGui::TextUnformatted(item.function.c_str());
+            ImGui::TableSetColumnIndex(2);
+            ImGui::TextUnformatted(item.arguments.c_str());
         }
         ImGui::EndTable();
     }

662682480aecbb9d8a8b89f5fd6a974b675718f4
Karczewski, Tom <Tom.Karczewski@amd.com>
2025-07-14 12:20:28 -0400

    Fetch and render callstack data (#202)
    * Added function to fetch call stack data.
    Added helper functions for retrieving string data.
    Refactored code to use string helper functions.
    
    * Fetch call stack data in event details.
    
    * Added getter  for callstack information.
    Removed setters.
    
    * Refactored event view to use horizontal splitter widget.
    
    Added callstack rendering.
    
    * Removing likely never to be used GetStringAsCharArray and FreeStringCharArray functions.
    
    * Disabled rendering borders in panel containers

--- src/view/src/rocprofvis_events_view.h
+++ src/view/src/rocprofvis_events_view.h
@@ -10,19 +10,24 @@ namespace RocProfVis
 namespace View
 {
 
-class EventsView : public RocCustomWidget
+class EventsView : public RocWidget
 {
 public:
     EventsView(DataProvider& dp);
     ~EventsView();
-    void ShowEventExtDataPanel(const std::vector<event_ext_data_t>& ext_data);
-    void ShowEventFlowInfoPanel(const std::vector<event_flow_data_t>& flow_data);
-    void Render();
+    void Render() override;
 
 private:
-    std::vector<std::string> events_;
+    void RenderEventExtData(const std::vector<event_ext_data_t>& ext_data);
+    void RenderEventFlowInfo(const std::vector<event_flow_data_t>& flow_data);
+    void RenderCallStackData(const std::vector<call_stack_data_t>& call_stack_data);
+
+    void RenderLeftPanel();
+    void RenderRightPanel();
+    
     DataProvider&            m_data_provider;
     uint64_t                 m_last_selected_event;
+    std::shared_ptr<HSplitContainer> m_h_spilt_container;
 };
 
 }  // namespace View

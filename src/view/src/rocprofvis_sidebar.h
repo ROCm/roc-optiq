f4a4fe941236dafd49d80ea82e99012af14b3a27
Chen, Derek <Derek.Chen@amd.com>
2025-07-18 16:38:30 -0400

    Sidebar topology details and track grouping.
    -Group tracks based on topology.
    -Add topology details.

--- src/view/src/rocprofvis_sidebar.h
+++ src/view/src/rocprofvis_sidebar.h
@@ -2,13 +2,11 @@
 
 #pragma once
 #include "imgui.h"
-#include "rocprofvis_timeline_view.h"
-// #include "rocprofvis_structs.h"
 #include "rocprofvis_data_provider.h"
+#include "rocprofvis_event_manager.h"
 #include "rocprofvis_settings.h"
 #include "rocprofvis_view_structs.h"
 #include "widgets/rocprofvis_widget.h"
-#include <map>
 #include <vector>
 
 namespace RocProfVis
@@ -19,19 +17,93 @@ namespace View
 class SideBar : public RocWidget
 {
 public:
-    SideBar(DataProvider& dp);
+    SideBar(DataProvider& dp, std::vector<rocprofvis_graph_t>* graphs);
     ~SideBar();
     virtual void Render();
-
-    void SetGraphs(std::vector<rocprofvis_graph_t>* graphs);
-    void ConstructTree();
+    void         Update();
 
 private:
-    int                              m_dropdown_select;
+    struct InfoTable
+    {
+        struct Cell
+        {
+            std::string data;
+            bool        expand;
+        };
+        std::vector<std::vector<Cell>> cells;
+    };
+    struct Queue
+    {
+        const queue_info_t* info;
+        InfoTable           info_table;
+        uint64_t            graph_index;
+    };
+    struct Thread
+    {
+        const thread_info_t* info;
+        InfoTable            info_table;
+        uint64_t             graph_index;
+    };
+    struct Counter
+    {
+        const counter_info_t* info;
+        InfoTable             info_table;
+        uint64_t              graph_index;
+    };
+    struct Process
+    {
+        const process_info_t*                  info;
+        InfoTable                              info_table;
+        std::string                            header;
+        std::string                            queue_header;
+        std::vector<Queue>                     queues;
+        std::unordered_map<uint64_t, Queue*>   queue_lut;
+        std::string                            thread_header;
+        std::vector<Thread>                    threads;
+        std::unordered_map<uint64_t, Thread*>  thread_lut;
+        std::string                            counter_header;
+        std::vector<Counter>                   counters;
+        std::unordered_map<uint64_t, Counter*> counter_lut;
+    };
+    struct Node
+    {
+        const node_info_t*                     info;
+        InfoTable                              info_table;
+        std::string                            process_header;
+        std::vector<Process>                   processes;
+        std::unordered_map<uint64_t, Process*> process_lut;
+    };
+    struct Project
+    {
+        std::string                         header;
+        std::string                         node_header;
+        std::vector<Node>                   nodes;
+        std::unordered_map<uint64_t, Node*> node_lut;
+        std::vector<uint64_t>               uncategorized_graph_indices;
+    };
+
+    /*
+     * Generates topology tree and builds UI strings.
+     * Runs once on trace to load to avoid repeating operations on static data.
+     */
+    void UpdateTopology();
+    /*
+     * Binds tree nodes to a timeline view graph.
+     * Runs on graph index change.
+     */
+    void UpdateGraphs();
+
+    void RenderTable(InfoTable& table);
+    void RenderTrackItem(const int& index);
+
     std::vector<rocprofvis_graph_t>* m_graphs;
 
-    Settings&     m_settings;
-    DataProvider& m_data_provider;
+    Settings&                       m_settings;
+    DataProvider&                   m_data_provider;
+    Project                         m_project;
+    bool                            m_topology_dirty;
+    bool                            m_graphs_dirty;
+    EventManager::SubscriptionToken m_metadata_changed_event_token;
 };
 
 }  // namespace View

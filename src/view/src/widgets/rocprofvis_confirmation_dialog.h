bfe0958431811accbc9d5553b1bc9facbf6d13ba
Tom <tkarczew@amd.com>
2025-07-31 16:30:07 -0400

    Add confirmation dialog implementation

---
+++ src/view/src/widgets/rocprofvis_confirmation_dialog.h
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#pragma once
+
+#include "imgui.h"
+#include <string>
+#include <functional>
+
+namespace RocProfVis
+{
+namespace View
+{
+
+class ConfirmationDialog {
+public:
+    void Show(const std::string& title, const std::string& message, std::function<void()> on_confirm_callback);
+    void Render();
+
+private:
+    std::string m_title;
+    std::string m_message;
+    std::function<void()> m_on_confirm;
+    bool m_should_open = false;
+};
+
+}  // namespace View
+}  // namespace RocProfVis
+

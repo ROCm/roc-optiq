17268e1530993dd86892b3ca8cac3d72ba0a745e
Vladimir Stempen <Vladimir.Stempen@amd.com>
2025-08-08 00:29:52 -0400

    Fixed SWDEV-548884 - GPU tracks return no data in rpd files (#266)
    Co-authored-by: Vladimir Stempen <vstempen@amd.com>

--- src/model/src/database/rocprofvis_db_rocpd.cpp
+++ src/model/src/database/rocprofvis_db_rocpd.cpp
@@ -223,7 +223,7 @@ rocprofvis_dm_result_t  RocpdDatabase::ReadTraceMetadata(Future* future)
                         // Level query
                         "select 2 as op, start, end, id, 0, gpuId, queueId, 0 as const from rocpd_op ",
                         // Slice query by queue
-                        "select 2 as op, start, end, opType_id, description_id, id, 0, gpuId, queueId , L.level as level, 0 as const from rocpd_op LEFT JOIN event_levels_op L ON id = L.eid ",
+                        "select 2 as op, start, end, opType_id, description_id, id, 0, gpuId, queueId , L.level as level from rocpd_op LEFT JOIN event_levels_op L ON id = L.eid ",
                         // Slice query by stream
                         "",
                         // Table query

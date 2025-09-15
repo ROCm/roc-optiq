dd6718c621c5a456ce7d6b2be203309c7a230204
Wimalasuriyan, Sajeeth <Sajeeth.Wimalasuriyan@amd.com>
2025-09-15 16:56:35 -0400

    Got icons working on toolbar.

--- src/view/src/icons/rocprovfis_icon_defines.h
+++ src/view/src/icons/rocprovfis_icon_defines.h
@@ -4,26 +4,20 @@ namespace RocProfVis
 {
 namespace View
 {
-	constexpr ImWchar icon_ranges[] = {
-		0xF128, 0xF128,
-		0xF133, 0xF133,
-		0xF13D, 0xF13D,
-		0xF267, 0xF267,
-		0xF306, 0xF306,
-		0xF35C, 0xF35C,
-		0xF3A8, 0xF3A8,
-		0xF424, 0xF424,
-		0
-	};
+constexpr ImWchar icon_ranges[] = { 0xF128, 0xF128, 0xF133, 0xF133, 0xF13D, 0xF13D,
+                                    0xF267, 0xF267, 0xF306, 0xF306, 0xF35C, 0xF35C,
+                                    0xF3A8, 0xF3A8, 0xF424, 0xF424, 0xF218, 0xF218,
+    0 };
 
-	constexpr char* ICON_X_CIRCLED     = u8"\uF128";
-	constexpr char* ICON_EYE           = u8"\uF133";
-	constexpr char* ICON_GEAR          = u8"\uF13D";
-	constexpr char* ICON_ARROWS_SHRINK = u8"\uF267";
-	constexpr char* ICON_EYE_SLASH     = u8"\uF306";
-	constexpr char* ICON_GRID          = u8"\uF35C";
-	constexpr char* ICON_ARROWS_CYCLE  = u8"\uF3A8";
-	constexpr char* ICON_EYE_THIN  	   = u8"\uF424";
+constexpr char* ICON_X_CIRCLED     = u8"\uF128";
+constexpr char* ICON_EYE           = u8"\uF133";
+constexpr char* ICON_GEAR          = u8"\uF13D";
+constexpr char* ICON_ARROWS_SHRINK = u8"\uF267";
+constexpr char* ICON_EYE_SLASH     = u8"\uF306";
+constexpr char* ICON_GRID          = u8"\uF35C";
+constexpr char* ICON_ARROWS_CYCLE  = u8"\uF3A8";
+constexpr char* ICON_EYE_THIN      = u8"\uF424";
+constexpr char* ICON_ADD_NOTE      = u8"\uF218";
 
 }  // namespace View
 }  // namespace RocProfVis

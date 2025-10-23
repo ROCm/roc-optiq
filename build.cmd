f775bb67ed27e5b577c558f7a06f7f3c452683bf
Osi Request <129049552+amd-orequest@users.noreply.github.com>
2025-10-23 05:00:22 -0700

    Update build.cmd
    Updated build.cmd file

--- build.cmd
+++ build.cmd
@@ -1,28 +1,44 @@
+@echo off
+
 REM THIS LINE NEEDED TO SUPPORT CRUISECONTROL
 SET DEVROOT=%CD%
-if %ERRORLEVEL%==0 ECHO. > %DevRoot%\success
+SET VISUALIZER_FILE_NAME=rocprof-visualizer
 
-REM	Installer Reqs
+if %ERRORLEVEL%==0 ECHO. > %DEVROOT%\success
+
+REM Installer Reqs
 REM ***************
 REM %DEVROOT% is a variable used during build, signifying rootfolder in a git repo.
 ECHO Build.cmd: Creating standard release folder structure
 CALL %DEVROOT%\CreateReleaseFolderStructure.cmd
 if %ERRORLEVEL% NEQ 0 GOTO BUILDISSUE
 
+REM === PowerShell translation begins ===
 
-REM Whatever files or folders copied to this folder during build will be picked up by installer and deployed on user's system exactly as it is.
-REM Use "%DEVROOT%\ReleaseCommon\Common\All\" - For Public release files , if any
-REM Use "%DEVROOT%\ReleaseInternal\Common\All\" - For Internal release files , if any
-REM Use "%DEVROOT%\ReleaseNDA\Common\All\" - For NDA release files , if any
-Echo Build.cmd: Copying files to standard folder structure
-copy /y "%DEVROOT%\"name and path in repo where the exe is present" "%DEVROOT%\ReleaseCommon\Common\All\"
+REM Create build directory
+IF NOT EXIST "%DEVROOT%\build" mkdir "%DEVROOT%\build"
+
+REM Run cmake build commands
+cmake --preset "x64-debug"
+cmake --build %DEVROOT%/build/x64-debug
+IF NOT EXIST "%DEVROOT%\build\x64-debug\Debug\%VISUALIZER_FILE_NAME%.exe" (
+    ECHO ❌ %VISUALIZER_FILE_NAME% was not built!
+    GOTO BUILDISSUE
+) ELSE (
+    ECHO ✅ %VISUALIZER_FILE_NAME% has been successfully built!
+)
+
+
+REM Copy the built executable to proper folder structure for release
+ECHO Build.cmd: Copying files to standard folder structure
+copy /y "%DEVROOT%\build\x64-debug\Debug\%VISUALIZER_FILE_NAME%.exe" "%DEVROOT%\ReleaseCommon\Common\All\"
 if %ERRORLEVEL% NEQ 0 GOTO BUILDISSUE
-REM Keep using md to create subfolders if needed.
-REM Example : md %DEVROOT%\ReleaseCommon\Common\All\bin
-REM copy /y "%DEVROOT%\somefile" "%DEVROOT%\ReleaseCommon\Common\All\bin"
-REM if %ERRORLEVEL% NEQ 0 GOTO BUILDISSUE
+
 
 :BUILDISSUE
-del /Q /F %DevRoot%\success
+del /Q /F %DEVROOT%\success
+ECHO Build failed!
+GOTO END
 
 :END
+ECHO Build completed.

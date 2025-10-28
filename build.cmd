53aa89e1510779e6d9567e7e4b64ff76b8d804f8
Osi Request <129049552+amd-orequest@users.noreply.github.com>
2025-10-28 05:49:59 -0700

    U/sjayeshw/testbranch (#463)
    * Update build.cmd
    
    Updated build.cmd file
    
    * Update build.cmd
    
    Updated build.cmd file
    
    * Create version.pl
    
    Added version.pl file
    
    * Update build.cmd
    
    * Update build.cmd
    
    * Update build.cmd
    
    * Update build.cmd
    
    updated build.cmd

--- build.cmd
+++ build.cmd
@@ -6,12 +6,6 @@ SET VISUALIZER_FILE_NAME=rocprof-visualizer
 
 if %ERRORLEVEL%==0 ECHO. > %DEVROOT%\success
 
-REM Installer Reqs
-REM ***************
-REM %DEVROOT% is a variable used during build, signifying rootfolder in a git repo.
-
-REM === PowerShell translation begins ===
-
 REM Create build directory
 IF NOT EXIST "%DEVROOT%\build" mkdir "%DEVROOT%\build"
 
@@ -32,7 +26,7 @@ if %ERRORLEVEL% NEQ 0 GOTO BUILDISSUE
 
 REM Copy the built executable to proper folder structure for release
 ECHO Build.cmd: Copying files to standard folder structure
-copy /y "%DEVROOT%\build\x64-debug\Debug\%VISUALIZER_FILE_NAME%.exe" "%DEVROOT%\ReleaseCommon\Common\All\"
+copy /y "%DEVROOT%\build\x64-release\Release\%VISUALIZER_FILE_NAME%.exe" "%DEVROOT%\ReleaseCommon\Common\All\"
 if %ERRORLEVEL% NEQ 0 GOTO BUILDISSUE
 
 GOTO END

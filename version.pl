1eb5e09e04ee688a5e1f924e9b0e4fdeb2e7c600
Tom <tkarczew@amd.com>
2025-10-31 10:03:51 -0400

    Updated version in installer script

--- version.pl
+++ version.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl
 
 $build = $ENV{'BNumber'};
+$version = "0.6.2.$build";
 
 open(CMD, "> AMDVersion.cmd") or die "can't open $newfile: $!";
-print(CMD "SET PKG_VERSION=1.0.0.$build\n");
-print(CMD "SET LABEL=1.0.0.$build\n");
+print(CMD "SET PKG_VERSION=$version\n");
+print(CMD "SET LABEL=$version\n");
 print(CMD "SET BuildNumber=$build\n");
 close(CMD);
 print "BuildVersionString: Dropped AMDVersion.cmd\n"; 
-print("BuildVersionString: Setup Version will be 1.0.0.$build\n");
+print("BuildVersionString: Setup Version will be $version\n");

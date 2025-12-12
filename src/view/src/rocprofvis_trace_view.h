ce05f98ca692cd8a7f0b0445eda0873a37abf36b
Wimalasuriyan, Sajeeth <Sajeeth.Wimalasuriyan@amd.com>
2025-12-12 12:51:50 -0500

    fix: corrected loading popout behavior and redesigned the UI for it.

--- src/view/src/rocprofvis_trace_view.h
+++ src/view/src/rocprofvis_trace_view.h
@@ -93,6 +93,7 @@ private:
     DataProvider m_data_provider;
     bool         m_view_created;
     bool         m_open_loading_popup;
+    bool         m_is_tab_active;
 
     SettingsManager& m_settings_manager;
 

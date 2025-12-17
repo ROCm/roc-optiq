e3efe96de2e7f0c188e260d607ebc7204509e231
Wimalasuriyan, Sajeeth <Sajeeth.Wimalasuriyan@amd.com>
2025-12-17 15:55:13 -0500

    refactor: moved comments around and addressed redundancy in code.

--- thirdparty/glimmer/src/glimmer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "types.h"
-#include "renderer.h"
-#include "style.h"
-#include "layout.h"
-#include "widgets.h"
-#include "libs/inc/implot/implot.h"
-#include "libs/inc/implot/implot_internal.h"

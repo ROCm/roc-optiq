1197989dcbac5643bd2c2f6eb357d01ee11efac9
Wimalasuriyan, Sajeeth <Sajeeth.Wimalasuriyan@amd.com>
2026-02-20 11:30:41 -0500

    Merged old table view to new redesigned API.

--- src/view/src/compute/rocprofvis_compute_view.h
+++ src/view/src/compute/rocprofvis_compute_view.h
@@ -70,8 +70,17 @@ public:
     ComputeTableView(DataProvider& data_provider);
     ~ComputeTableView(){};
 
-protected:
-    DataProvider& m_data_provider;
+    void Update() override;
+    void Render() override;
+
+private:
+    void RenderCategory(const AvailableMetrics::Category& cat);
+
+    DataProvider&                         m_data_provider;
+    uint32_t                              m_workload_id = 0;
+    uint32_t                              m_last_workload_id = 0;
+    std::unordered_set<uint32_t>          m_kernel_ids;
+    std::shared_ptr<TabContainer>         m_tabs;
 };
 
 class ComputeWorkloadView: public RocWidget

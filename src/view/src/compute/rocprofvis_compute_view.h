9dad8d4f6c20036863baba015deedb771cd92d2f
Jeremy Nugara <jnugara@amd.com>
2026-06-22 10:40:45 -0400

    test: compute_view_tab_switch via captured tab-header id; close File menu popup so it doesn't intercept later clicks

--- src/view/src/compute/rocprofvis_compute_view.h
+++ src/view/src/compute/rocprofvis_compute_view.h
@@ -34,6 +34,10 @@ public:
     std::shared_ptr<RocWidget> GetToolbar() override;
     std::optional<DataProviderCleanupWork> DetachProviderCleanup() override;
 
+#ifdef IMGUI_ENABLE_TEST_ENGINE
+    TabContainer* GetTabContainerForTest() { return m_tab_container.get(); }
+#endif
+
 private:
     void RenderToolbar();
     void RenderWorkloadSelection();

df165cbcb1eebf1bf42e05af1f710415e70cde44
Vu <trangvu2@amd.com>
2026-08-13 22:11:51 -0400

    docs: add v1.0.0 release notes, remove beta tag and 0.5.0 changelog

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ flavor options: rocm, rocm-docs-home, rocm-blogs, rocm-ds, instinct, ai-develope
 html_theme = "rocm_docs_theme"
 html_theme_options = {
     "flavor": "generic",
-    "header_title": "ROCm™ Optiq (Beta) 0.5.0",
+    "header_title": "ROCm™ Optiq 1.0.0",
     "header_link": "https://rocm.docs.amd.com/projects/roc-optiq/en/latest/",
     "version_list_link": "https://rocm.docs.amd.com/projects/roc-optiq/en/latest/release.html",
     "nav_secondary_items": {
@@ -43,10 +43,10 @@ all_article_info_author = ""
 #                      f.read())
 #    if not match:
 #        raise ValueError("VERSION not found!")
-version_number = "0.5.0"
+version_number = "1.0.0"
 
 # for PDF output on Read the Docs
-project = "ROCm Optiq (Beta)"
+project = "ROCm Optiq"
 author = "Advanced Micro Devices, Inc."
 copyright = "Copyright (c) 2026 Advanced Micro Devices, Inc. All rights reserved."
 version = version_number
@@ -73,4 +73,4 @@ extensions = [
 
 html_title = f"{project} {version_number} documentation"
 
-external_projects_current_project = "ROCm™ Optiq (Beta)"
+external_projects_current_project = "ROCm™ Optiq"
